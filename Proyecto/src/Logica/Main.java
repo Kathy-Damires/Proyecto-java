7ccffa5142d44f0f22dd77eb703290cd85f10df9
Alumno <jonathan>
2020-08-27 10:35:42 -0300

    Prueba de clase Usuario y subordinadas. Se cambia la variable Date a LocalDate

--- Proyecto/src/Logica/Main.java
+++ Proyecto/src/Logica/Main.java
@@ -1,10 +1,12 @@
 package Logica;
 
+import java.time.LocalDate;
+
 public class Main {
 
-	Estudiante estudiante1 = new Estudiante("53542683", "1234", "Jonathan", "Cembranos", "jona@gmail.com", 18/10/2020);
+	Estudiante estudiante1 = new Estudiante("53542683", "1234", "Jonathan", "Cembranos", "jona@gmail.com", LocalDate(02, 05, 2020));
 
-	
+
 
 }
 
