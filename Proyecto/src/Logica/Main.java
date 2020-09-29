3919dd01553b21f17c8a1693f891c7b0c350aed1
Alumno <jonathan>
2020-09-29 09:21:15 -0300

    mas de lo mismo 2

--- Proyecto/src/Logica/Main.java
+++ Proyecto/src/Logica/Main.java
@@ -5,18 +5,13 @@ import java.time.LocalDate;
 public class Main {
 	public static void main(String[] args) {
 
-		
-		
-		
-		
-		
 		// alta usuarios
 		// alta usuario1
 		Usuario u1 = new Usuario("53542683", "contrase;a", "nombre", "apellido", "Mail", LocalDate.of(1996, 3, 13));
 		// alta estudiante
 		Usuario e1 = new Estudiante("ci", "contraseña", "nombre", "apellido", "mail", LocalDate.of(2020, 3, 1),
 				Orientacion.TIC, Estado.ACTIVO, Generacion.PRIMERO);
-	
+
 		// alta docente
 		Usuario d1 = new Docente("ci", "contraseña", "nombre", "apellido", "mail", LocalDate.of(2020, 3, 1));
 
@@ -27,21 +22,22 @@ public class Main {
 		Inasistencia i1 = new Inasistencia(1, LocalDate.of(2019, 5, 15), TipoInasistencia.INJUSTIFICADA);
 
 		// Relaciones
-		Cursada c1 = new Cursada( 8, programacion);
-		((Estudiante)e1).addMateriasEnCurso(c1);
+		Cursada c1 = new Cursada(8, programacion);
+		((Estudiante) e1).addMateriasEnCurso(c1);
 	}
-	
-	//funciones 
-	public static void crearUsuario(String ci, String contraseña, String nombre, String apellido, String mail, LocalDate fecha) {
-		Usuario u1 = new Usuario(ci, contraseña, nombre, apellido, mail, LocalDate.of(1996, 3, 13));
+
+	// funciones
+	public static void crearUsuario(String ci, String contraseña, String nombre, String apellido, String mail,
+			LocalDate fecha) {
+		Usuario usuario = new Usuario(ci, contraseña, nombre, apellido, mail, LocalDate.of(1996, 3, 13));
 	}
-	
+
 	public static void crearMateria(String codigo, String nombre, Orientacion orientacion, Generacion generacion) {
 		Materia materia = new Materia(codigo, nombre, orientacion, generacion);
 	}
-	
-	public static void altaInasistencia() {
-		System.out.println("hola mundo");
+
+	public static void altaInasistencia(int cantidadHoras, LocalDate fecha, TipoInasistencia tipo, Materia materia,
+			Estudiante estudiante) {
+		Inasistencia inasistenci = new Inasistencia(cantidadHoras, fecha, tipo);
 	}
 }
-
