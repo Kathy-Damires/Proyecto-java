c64502c5049dfb88dacfd6db2370c7fac69ae8cd
Alumno <jonathan>
2020-11-20 11:24:12 -0300

    main

--- Proyecto/src/Logica/Main.java
+++ Proyecto/src/Logica/Main.java
@@ -1,40 +1,36 @@
 
 package Logica;
+
 import java.sql.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
 
+		// TODO Auto-generated method stub
+		Persistencia.Conn connect = new Persistencia.Conn();
 
+		Connection con = connect.conectarMySQL();
 
-	
-				// TODO Auto-generated method stub
-			Persistencia.Conn connect = new Persistencia.Conn();
-				
-				Connection con = connect.conectarMySQL();
-				
-				Statement s;
-				ResultSet rs;
-				
-				String tabla = "usuario";
-				
-				String query = "SELECT * FROM " + tabla;
-				
-				try {
-					s = con.createStatement();
-					rs = s.executeQuery(query);
+		Statement s;
+		ResultSet rs;
 
-					while (rs.next()) {
-						System.out.println(rs.getInt("ci") + " " + rs.getString("nombre") + " " + rs.getString("apellido"));
-					}
+		String tabla = "usuario";
 
-				}catch(SQLException ex) {
-					
-				}
-				
-			
+		String query = "SELECT * FROM " + tabla;
+
+		try {
+			s = con.createStatement();
+			rs = s.executeQuery(query);
+
+			while (rs.next()) {
+				System.out.println(rs.getInt("ci") + " " + rs.getString("nombre") + " " + rs.getString("apellido"));
+			}
+
+		} catch (SQLException ex) {
+
+		}
+
 	}
 
 }
